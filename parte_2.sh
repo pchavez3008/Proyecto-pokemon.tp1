00d3e41aebdd59097414aba0ef3fe7d3ce85813b
Pedro Chávez <pchavez3008@gmail.com>
2025-09-06 21:31:47 -0400

    Actualizo parte_2.sh con la versión final

--- parte_2.sh
+++ parte_2.sh
@@ -2,8 +2,12 @@
 
 while read nombre; do
   # Obtener id, altura y peso
-  info=$(awk -F',' -v n="$nombre" 'NR>1 && $2==n {print $1, $4*10, $5/10}' ./data/pokemon.csv)
-  [ -z "$info" ] && continue
+  info=$(awk -F',' -v n="$nombre" 'NR>1 && tolower($2)==tolower(n) {print $1, $4*10, $5/10}' ./data/pokemon.csv)
+  if [ -z "$info" ]; then
+    echo "Pokemon no encontrado: $nombre"
+    echo ""
+    continue
+  fi
   id=$(echo $info | cut -d' ' -f1)
   altura=$(echo $info | cut -d' ' -f2)
   peso=$(echo $info | cut -d' ' -f3)
@@ -14,10 +18,16 @@ while read nombre; do
   echo ""
   echo "Habilidades:"
 
-  # Obtener habilidades en español (local_language_id == 7)
-  awk -F',' -v pid="$id" 'NR>1 && $1==pid {print $2}' ./data/pokemon_abilities.csv | while read abid; do
-    habilidad=$(awk -F',' -v aid="$abid" 'NR>1 && $1==aid && $2==7 {print $3}' "./data/ability_names (1).csv")
-    [ -n "$habilidad" ] && echo "* $habilidad"
-  done
+  habilidades=$(awk -F',' -v pid="$id" 'NR>1 && $1==pid {print $2}' ./data/pokemon_abilities.csv)
+  if [ -z "$habilidades" ]; then
+    echo "* Sin habilidades registradas"
+  else
+    while read abid; do
+      habilidad=$(awk -F',' -v aid="$abid" 'NR>1 && $1==aid && $2==7 {print $3}' "./data/ability_names (1).csv")
+      if [ -n "$habilidad" ]; then
+        echo "* $habilidad"
+      fi
+    done <<< "$habilidades"
+  fi
   echo ""
 done
